9ef9f3a8a951a3a6cc6a9d48950380a052d448f2
Rajesh Praveen Gundabathula <rgundabathula@CORP.MIRACLESOFT.COM>
2019-04-29 20:54:15 +0530

    remove something in master

--- report_data/showHide.js
+++
@@ -1,7 +0,0 @@
-function hide(identifiant) { 
-      if (document.getElementById(identifiant).className == "hidden") { 
-         document.getElementById(identifiant).className = "visible"; 
-      } else { 
-         document.getElementById(identifiant).className = "hidden"; 
-      } 
-   }
